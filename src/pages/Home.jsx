a996997ddfd2efdfd06af76224aa0864c212527b
arturrezendec <arturrezendec@gmail.com>
2022-02-10 21:27:32 +1100

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,48 @@
 import React, { useEffect, useState } from 'react'
-import { getDocs, collection } from 'firebase/firestore'
+import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase-config'
+import { auth } from '../firebase-config'
 
 
-function Home() {
+function Home({isAuth}) {
     const [postLists, setPostList] = useState([]);
     const postsCollectionRef = collection(db, 'posts');
+
+    const deletePost = async (id) => {
+        const postDoc = doc(db, 'posts', id);
+        await deleteDoc(postDoc)
+    };
 
     useEffect(() => {
         const getPosts = async () => {
             const data = await getDocs(postsCollectionRef);
+            console.log(data);
             setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
         }
         getPosts();
-    });
+    },[deletePost]);
+        
+    
 
     return (
         <div className='homePage'>
-            {''}
             {postLists.map((post) => {
-                return <div className='post'>
+                return (
+                <div className='post'>
                     <div className='postHeader'>
-                        {''}
                         <div className='postTitle'>
                             <h1>{post.title}</h1>
+                            {isAuth && post.author.id === auth.currentUser.uid && 
+                            ( <button className='delete-post-btn' 
+                            onClick={() => deletePost(post.id)}>Delete</button>
+                            )}
                         </div>
                         <div className='postText'>
                             <p>{post.postText}</p>
                         </div>
                     </div>
-                </div>;
+                </div>
+                );
             })}
         </div>
     );
